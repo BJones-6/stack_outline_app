3764d770f2e37551490c00263c50fdc7bafcae5b
Bryson Jones <z955v229@wichita.edu>
2026-05-08 19:47:25 -0500

    Added basic CRUD functionality for all tables, new placeholder sections in index.html, and added forms folder

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template
+from flask import request, redirect, url_for
+from .extensions import db
+from datetime import datetime
 
 from .models import Trader, Account, Asset, Trade
 
@@ -6,7 +9,7 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    traders= Trader.query.all()
+    traders = Trader.query.all()
     return render_template("index.html", traders=traders)
 
 
@@ -14,9 +17,7 @@ def index():
 
 @main.route("/trader/create", methods=["POST"])
 def create_trader():
-    from flask import request, redirect, url_for
-    from .extensions import db
-    from datetime import datetime
+    
 
     new_trader = Trader(
         TraderID=request.form["TraderID"],
@@ -34,7 +35,7 @@ def create_trader():
 
 @main.route("/trader/delete/<id>")
 def delete_trader(id):
-    from .extensions import db
+
 
     trader = Trader.query.get_or_404(id)
     db.session.delete(trader)
@@ -47,8 +48,7 @@ def delete_trader(id):
 @main.route("/trader/update/<id>", methods=["POST"])
 def update_trader(id):
 
-    from flask import request
-    from .extensions import db
+
 
     trader = Trader.query.get_or_404(id)
 
@@ -67,4 +67,206 @@ def trader_detail(id):
     trader = Trader.query.get_or_404(id)
     return render_template("trader.html", trader=trader)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@main.route("/account/create", methods=["POST"])
+def create_account():
+
+
+    new_account = Account(
+        AccountID=request.form["AccountID"],
+        TraderID=request.form["TraderID"],
+        Balance=request.form["Balance"],
+        Status=request.form["Status"],
+        OpenDate=datetime.today().date(),
+        LastUpdated=datetime.today().date()
+    )
     
+        
+
+
+
+    db.session.add(new_account)
+    db.session.commit()
+
+
+    return redirect(url_for("main.index"))
+
+@main.route("/account/delete/<id>")
+def delete_account(id):
+    account = Account.query.get_or_404(id)
+
+    db.session.delete(account)
+    db.session.commit
+
+    return "Deleted"
+
+
+@main.route("/account/update/<id>", methods=["POST"])
+def update_account(id):
+    account = Account.query.get_or_404(id)
+
+    account.Status = request.form["Status"]
+
+    db.session.commit()
+
+    return "Updated"
+
+
+
+@main.route("/account/<id>")
+def account_detail(id):
+    account = Account.query.get_or_404(id)
+
+    return render_template("account.html", account=account)
+
+
+
+
+
+
+
+
+
+
+
+
+@main.route("/asset/create", methods=["POST"])
+def create_asset():
+    new_asset = Asset(
+        AssetID = request.form["AssetID"],
+        Symbol = request.form["Symbol"],
+        CurrentPrice = request.form["CurrentPrice"],
+        AssetName = request.form["AssetName"],
+        CreationDate = datetime.today().date(),
+        LastUpdated = datetime.today().date()
+    )
+
+
+    db.session.add(new_asset)
+    db.session.commit()
+
+    return redirect(url_for("main.index"))
+
+
+
+@main.route("/asset/delete/<id>")
+def delete_asset(id):
+    asset = Asset.query.get_or_404(id)
+
+    db.session.delete(asset)
+    db.session.commit()
+
+    return "Deleted"
+
+
+
+@main.route("/asset/update/<id>", methods=["POST"])
+def update_asset(id):
+    asset = Asset.query.get_or_404(id)
+
+    asset.Symbol = request.form["Symbol"]
+
+    asset.CurrentPrice = request.form["CurrentPrice"]
+
+    asset.AssetName = request.form["AssetName"]
+
+    asset.LastUpdated = datetime.today().date()
+
+    return "Updated"
+
+
+@main.route("/asset/<id>")
+def detail_asset(id):
+    asset = Asset.query.get_or_404(id)
+
+    return render_template("asset.html", asset=asset)
+
+
+
+
+
+
+
+
+@main.route("/trade/create", methods=["POST"])
+def create_trade():
+    new_trade = Trade(
+        TradeID = request.form["TradeID"],
+        AccountID = request.form["AccountID"],
+        AssetID = request.form["AssetID"],
+        TradeType = request.form["TradeType"],
+        Quantity = request.form["Quantity"],
+        EntryPrice = request.form["EntryPrice"],
+        ExitPrice = request.form["ExitPrice"],
+        EntryDate = request.form["EntryDate"],
+        ExitDate = request.form["ExitDate"],
+        Status = request.form["Status"],
+        LastUpdated = datetime.today().date()
+    )
+
+    db.session.add(new_trade)
+
+    db.session.commit()
+
+    return redirect(url_for("main.index"))
+
+
+@main.route("/trade/delete/<id>")
+def delete_trade(id):
+
+    trade = Trade.query.get_or_404(id)
+
+    db.session.delete(trade)
+    db.session.commit()
+
+    return "Deleted"
+
+
+
+@main.route("/trade/update/<id>", methods=["POST"])
+def update_trade(id):
+    trade = Trade.query.get_or_404(id)
+
+    trade.ExitPrice = request.form["ExitPrice"]
+
+    trade.ExitDate = datetime.today().date()
+
+    trade.Status = request.form["Status"]
+
+    trade.LastUpdated = datetime.today().date()
+
+
+
+    return redirect(url_for("main.index"))
+
+
+
+
+@main.route("/trade/<id>")
+def detail_trade(id):
+    trade = Trade.query.get_or_404(id)
+
+    return render_template("trade.html", trade=trade)
+
+
+
+
+
+
+
+
+
